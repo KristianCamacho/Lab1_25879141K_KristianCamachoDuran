17567468933138d3435eff388aadcf513574a875
KristianCamacho <kristian.camacho@usach.cl>
2024-10-19 21:21:57 -0400

    Create TDA_Player_25879141k_CamachoDuran.rkt

---
+++ TDA_Player_25879141k_CamachoDuran.rkt
@@ -0,0 +1,8 @@
+#lang racket
+(provide (all-defined-out))
+
+;;REQUERIMIENTO 2: Función que permite crear un player
+;Dom: id (int) X name (string) X color (string) X wins (int) X losses (int) X draws (int) X remaining-pieces (int)
+;Rec: player
+;Tipo de recursividad: No aplica
+;CONSTRUCTOR
