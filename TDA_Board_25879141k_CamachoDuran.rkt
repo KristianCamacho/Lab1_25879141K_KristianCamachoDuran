5a93c0f672f907b3833cab40c9707d5085cdccd1
KristianCamacho <kristian.camacho@usach.cl>
2024-10-26 20:13:03 -0400

    Requerimiento 6 board-set-play-piece

--- TDA_Board_25879141k_CamachoDuran.rkt
+++ TDA_Board_25879141k_CamachoDuran.rkt
@@ -30,3 +30,20 @@
         (if (row? (car b))#t
             (board? (cdr b)))))
   (board? board))
+
+;REQUERIMIENTO 6: Función que permite Jugar una ficha en el tablero
+;Dom: board (board) X column (int) X piece (piece)
+;Rec: board
+;Tipo de recursividad: Rec Natural
+
+(define (board-set-play-piece board column piece)
+  (define (row? row)
+    (and (not (null? row)) (eq? (list-ref row column) 0)))
+  (define (set-piece rows column piece)
+    (if (null? rows) '()  
+        (let ((current-row (car rows)))
+          (if (row? current-row)  
+              (cons (list-set current-row column piece) (cdr rows))  
+              (cons current-row (set-piece (cdr rows) column piece))))))
+  
+  (reverse (set-piece (reverse board) column piece)))
